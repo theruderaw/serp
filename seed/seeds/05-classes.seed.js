c2e26bf781377dabfcc57d07249c272842d4c15e
theruderaw <theruderaw678@gmail.com>
2026-07-17 04:29:22 +0530

    Added complete sidebar with school management. Settings to be done next

--- seed/seeds/05-classes.seed.js
+++ seed/seeds/05-classes.seed.js
@@ -18,7 +18,6 @@ export async function seedClasses(client, ctx) {
         VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
         RETURNING *;
     `;
-    console.log(ctx.user)
 
     for (const cls of classes) {
         const { rows } = await client.query(sql, [
